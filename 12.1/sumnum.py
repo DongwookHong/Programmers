9f81e310c972de987d5f33cfbda13e6dca54f76f
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2022-12-01 10:53:08 +0900

    Update sumnum.py

--- 12.1/sumnum.py
+++ 12.1/sumnum.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     answer = 0
     for i in range(1,10):
-        if  i not in numbers:
+        if  i not in numbers: // not in i 가  number  에 들어가있 지않으 함수찾기 
             answer += i
     return answer
