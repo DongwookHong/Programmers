e0d612e00e33859501a7fca12668e6a7a10e376a
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2022-12-01 10:52:19 +0900

    Update money.py

--- 12.1/money.py
+++ 12.1/money.py
@@ -1,6 +1,6 @@
 def solution(price, money, count):
     sum =0
-    for i in range(1,count+1):
+    for i in range(1,count+1): // 파이썬 for문  1 부터 cnt -1 까지돈다 
         sum += price * i
     if sum >= money:
         answer = sum - money
