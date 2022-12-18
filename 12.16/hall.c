d50bce36f69bc487c519027faebc8b583225d510
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2022-12-18 21:33:04 +0900

    Update hall.c

--- 12.16/hall.c
+++ 12.16/hall.c
@@ -2,9 +2,48 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// score_len은 배열 score의 길이입니다.
+void swap_index(int *a, int *b)
+{
+    int temp=0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort_me(int *str, int n)
+{
+    int temp;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - 1 - i; j++)
+            if (str[j] > str[j + 1])
+                swap_index(&str[j],&str[j+1]);
+}
+    
+    
+
 int* solution(int k, int score[], size_t score_len) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(1);
+    
+    int *answer = (int*)malloc(sizeof(int)*score_len);
+   
+    for(int i=0; i<score_len;i++ )
+    {
+        if(i<k)
+        {      
+              
+                    sort_me(score,i+1);
+                    answer[i]=score[0];
+        }
+        else
+        {
+            if(score[0] < score[i])
+            {
+                swap_index(&score[0],&score[i]);
+                sort_me(score,k);
+                answer[i] = score[0];
+            }
+            else
+                answer[i] = score[0];
+        }
+    }
     return answer;
 }
