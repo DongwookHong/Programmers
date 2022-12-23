cdf58a138a3c455095ea5d3c7f410d524c465ac1
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2022-12-23 13:12:54 +0900

    Update needfix.c

--- lvl2/12.23/needfix.c
+++ lvl2/12.23/needfix.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 
+int strtonum(char *str)
+{
+    int i =0;
+    int cnt=0;
+    int sum,sum2,sum3;
+    
+    
+    for(int i =0 ; i < strlen(str); i++)
+    {
+        sum =0;
+        sum2=0;
+        sum3 =0;
+        int k = i;
+        for (int j =0; j <strlen(str);j++)
+        {
+           if(k == strlen(str))
+                k=0;
+            if(str[k] == '(')
+                sum +=1;
+            else if(str[k] == ')')
+                sum -=1;
+            else if(str[k] == '{')
+                sum2 +=2;
+            else if(str[k] == '}')
+                sum2 -= 2;
+            else if(str[k] == '[')
+                sum3 +=3;
+            else if(str[k] == ']')
+                sum3 -=3;
+            
+            if(sum < 0 || sum2 < 0 || sum3 < 0)
+                break;
+            k++;
+        }
+        
+        if (sum == 0 && sum2 ==0 && sum3 == 0 )
+            cnt++;
+    }
+   return cnt;
+}
+
+
+int solution(const char* s) {
+    char *copy = strdup(s);
+    int cnt =strtonum(copy);
+
+    
+    return cnt;
+}
