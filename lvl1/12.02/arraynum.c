d5c939d90220f924792b3e9ccfd7383fb92821a3
DongwookHong <108330844+DongwookHong@users.noreply.github.com>
2022-12-30 11:00:52 +0900

    Update arraynum.c

--- lvl1/12.02/arraynum.c
+++ lvl1/12.02/arraynum.c
@@ -7,42 +7,32 @@
 
 
 int solution(const char* s) {
-    char num[20][20] = { "zero", "one", "two", "three","four", "five", "six" , "seven","eight","nine"};
-    char answer[50] = {0,};
+    char num[10][6] = { "zero", "one", "two", "three","four", "five", "six" , "seven","eight","nine"};
+    char answer[22] = {0,};
     char *cpy;
 
     cpy = strdup(s);
     int len = strlen(cpy);
     int k=0;
     
-    for(int i =0; i < len-1 ; i++)
+    for(int i =0; i < len; i++)
     {
-        if(s[i]>= '0' && s[i] <= '9')
-            answer[k++] = s[i];
+        if(cpy[i]>= '0' && cpy[i] <= '9')
+            answer[k++] = cpy[i];
 
-        for(int j = 0;  j < 10; ++j)
+        for(int j = 0;  j < 10; j++)
         {
-            if (!strncmp(&s[i], num[j],strlen(num[j])))
+            if (!strncmp(&cpy[i], num[j],strlen(num[j])))
             {
                 answer[k++] = j + '0';
-                // break;
+                break;
                 // i +=strlen(num[j]);
             }
         }
     }
  
-int y =0;
-while(answer[y])
-    printf("%d ", answer[y++]);
-
 int a;
+
  a = atoi(answer);
     return a;
-}
-
-
-int main()
-{
-    char * a = "one4seveneightnine";
-    printf("%d" , solution(a));
 }
